54ac464455d2aa63b4b8a58724576388f1df8712
zeba-shaikh2207 <zeba.shaikh2207@gmail.com>
2020-07-08 20:25:51 +0530

    Add Platform

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,300,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -59,4 +60,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
+    
 }
